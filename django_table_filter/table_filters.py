d965267abce1a0df772faf9b2b52d6ef9465dd7f
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-08-04 15:05:01 +0430

    set_base_column_filters and check_column_filters methods created
    first method categorizes column filters
    
    second method checks column filters if column exists or don't

--- django_table_filter/table_filters.py
+++ django_table_filter/table_filters.py
@@ -1,4 +1,5 @@
 from .tables import Table
+from .column_filters import ColumnFilter
 
 
 class TableFilterOption:
@@ -48,7 +49,38 @@ class TableFilterMetaclass(type):
         if attrs.get('Meta') is None:
             raise AttributeError(f'{name} without Meta class')
         attrs['_meta'] = opt = TableFilterOption(options=attrs.get('Meta'), class_name=name)
+        cls.set_base_column_filters(attrs)
+        base_column_filters = attrs['base_column_filters']
+        cls.check_column_filters(base_column_filters, opt.table)
         return type.__new__(cls, name, bases, attrs)
+
+    @staticmethod
+    def set_base_column_filters(attrs):
+        """
+        it finds ColumnFilter instance and sets them to "base_column_filters"
+
+        :param attrs:
+        :return:
+        """
+        base_column_filters = {}
+        for key, value in attrs.items():
+            if isinstance(value, ColumnFilter):
+                base_column_filters[key] = value
+                del attrs[key]
+        attrs['base_column_filters'] = base_column_filters
+
+    @staticmethod
+    def check_column_filters(base_column_filters, table):
+        """
+        it checks column filter with column in table
+
+        :param base_column_filters:
+        :param table:
+        :return:
+        """
+        for key, value in base_column_filters.items():
+            if table.base_columns.get(key) is None:
+                raise AttributeError(f'defined column filter "{key}" without column in {table}')
 
 
 class TableFilter(metaclass=TableFilterMetaclass):
