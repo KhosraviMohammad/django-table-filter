b96f43a34ed66ff3de08b3b311cf880d0b766c9c
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-08-18 11:12:50 +0430

    generate_column_filter body function is created

--- django_table_filter/conf.py
+++ django_table_filter/conf.py
@@ -1,4 +1,9 @@
+from django.db import models
+from django_filters import filters
+from .column_filters import ColumnFilter
 
 
 def generate_column_filter(*, column, table, model, split_column_names, name_and_fields, fields_and_models):
-    ...
+    if isinstance(split_column_names[len(name_and_fields) - 1], models.CharField):
+        return ColumnFilter({column: filters.CharFilter()})
+    return ColumnFilter()
