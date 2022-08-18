40d0e777b176b0a4ba2e1f10924e2f136869eae6
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-08-18 10:50:51 +0430

    None condition is added to columnFilter.__init__

--- django_table_filter/column_filters.py
+++ django_table_filter/column_filters.py
@@ -8,5 +8,7 @@ class ColumnFilter:
                 if not isinstance(value, Filter):
                     raise TypeError(f'In "filters" items must be instance of {Filter} class')
             self.filters = filters
+        elif filters is None:
+            self.filters = {}
         else:
             raise TypeError('Filters must be dictionary object')
