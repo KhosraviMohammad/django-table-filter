b63e9100ceb1c13c31a56e48d33c1ffc2f8e25d0
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-08-25 11:10:58 +0430

    set_table_filter method created

--- django_table_filter/tables.py
+++ django_table_filter/tables.py
@@ -7,6 +7,17 @@ class TableMetaclass(tables.DeclarativeColumnsMetaclass):
 
 
 class Table(tables.Table, metaclass=TableMetaclass):
-    
+
     def __init__(self, *args, **kwargs):
         super(Table, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def set_table_filter(table, table_filter):
+        """
+        it sets table_filter as attribute to table.table_filter
+
+        :param table:
+        :param table_filter:
+        :return:
+        """
+        table.table_filter = table_filter
