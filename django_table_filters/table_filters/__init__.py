81ebad121b8a2b91ad52bd60625c79dd52f20602
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-09-29 17:49:59 +0330

    some modules are imported in table_filters.__init__.py

--- django_table_filters/table_filters/__init__.py
+++ django_table_filters/table_filters/__init__.py
@@ -1,3 +1,7 @@
 from .column_filters import ColumnFilter
 from .table_filters import TableFilter
 from .tables import Table
+from django_tables2.columns import Column
+from django_filters import filterset
+from django_filters import filters
+
