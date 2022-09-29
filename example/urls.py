3dddab030b7548e5d977c732b5f7fa1a3351cd76
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-09-29 12:29:40 +0330

    new view named library_view is created

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'example'
 
 urlpatterns = [
+    path('', views.library_view, 'library_view')
 ]
--- example/views.py
+++ example/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def library_view(request):
+    context = {}
+    return render(request, template_name='example/library.html', context=context)
