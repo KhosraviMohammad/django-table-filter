a201d332b94f575912d5e71933059c6e7395ec31
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-10-04 18:20:09 +0330

    new LibraryTableFilter is created

--- example/tables.py
+++ example/tables.py
@@ -1,9 +1,14 @@
-from re import template
 from django_table_filters.table_filters import Table, TableFilter, ColumnFilter, Column
 from example import models
+
 
 class LibraryTable(Table):
     class Meta:
         model = models.Library
-        template_name  = 'django_tables2/bootstrap4.html'
+        template_name = 'django_tables2/bootstrap4.html'
         fields = ['name', 'location', 'users']
+
+
+class LibraryTableFilter(TableFilter):
+    class Meta:
+        table = LibraryTable
