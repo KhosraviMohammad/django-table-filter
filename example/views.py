d6be040e774ea643c5370d0989e2dab21fc529e7
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-10-05 17:34:28 +0330

    library_view is changed for using table filter ability

--- example/views.py
+++ example/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from .tables import LibraryTable
 from . import models
 
+
 # Create your views here.
 
 def library_view(request):
     library_qs = models.Library.objects.prefetch_related('users').all()
-    table = LibraryTable(data=library_qs)
+    table = LibraryTable(data=library_qs, table_filter_activation=True, request=request)
     context = {
         'table': table,
-        }
+    }
     return render(request, template_name='example/library.html', context=context)
