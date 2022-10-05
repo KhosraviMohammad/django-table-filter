63e26b9c1889e5b7a1bf185426829c1050d93358
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-10-05 18:33:58 +0330

    generate_column_filter function is changed to see some result

--- django_table_filters/table_filters/conf.py
+++ django_table_filters/table_filters/conf.py
@@ -4,6 +4,6 @@ from .column_filters import ColumnFilter
 
 
 def generate_column_filter(*, column, table, model, split_column_names, name_and_fields, fields_and_models):
-    if isinstance(split_column_names[len(name_and_fields) - 1], models.CharField):
-        return ColumnFilter({column: filters.CharFilter()})
-    return ColumnFilter()
+    # if isinstance(split_column_names[len(name_and_fields) - 1], models.CharField):
+    return ColumnFilter({split_column_names[0]: filters.CharFilter()})
+    # return ColumnFilter()
