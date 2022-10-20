7d6da318120ea6967710b7a66440d11f5989f8a6
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-10-20 12:09:42 +0330

    changes is for showing dropdown-filter

--- django_table_filters/table_filters/conf.py
+++ django_table_filters/table_filters/conf.py
@@ -10,24 +10,24 @@ def generate_column_filter(*, column, table, model, split_column_names, name_and
     filter_accessor = column.accessor
     column_filter = ColumnFilter()
     if last_field.choices is not None:
-        column_filter = ColumnFilter({filter_accessor: filters.ChoiceFilter(choices=last_field.choices, widget=forms.TextInput(attrs={'class': 'form-control'}))})
+        column_filter = ColumnFilter({filter_accessor: filters.ChoiceFilter(choices=last_field.choices, widget=forms.TextInput(attrs={'class': 'form-control input-filter'}))})
     elif isinstance(last_field, models.ManyToManyField):
         last_model = fields_and_models[last_field]
-        column_filter = ColumnFilter({filter_accessor: filters.ModelMultipleChoiceFilter(queryset=last_model.objects.all(), widget=forms.SelectMultiple(attrs={'class': 'form-control'}))})
+        column_filter = ColumnFilter({filter_accessor: filters.ModelMultipleChoiceFilter(queryset=last_model.objects.all(), widget=forms.SelectMultiple(attrs={'class': 'form-control input-filter'}))})
     elif isinstance(last_field, models.ForeignKey):
         last_model = fields_and_models[last_field]
-        column_filter = ColumnFilter({filter_accessor: filters.ModelChoiceFilter(queryset=last_model.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))})
+        column_filter = ColumnFilter({filter_accessor: filters.ModelChoiceFilter(queryset=last_model.objects.all(), widget=forms.Select(attrs={'class': 'form-control input-filter'}))})
     elif isinstance(last_field, models.CharField):
-        column_filter = ColumnFilter({filter_accessor: filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'class': 'form-control'}))})
+        column_filter = ColumnFilter({filter_accessor: filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'class': 'form-control input-filter'}))})
     elif isinstance(last_field, models.DateTimeField):
-        filter_from = filters.DateTimeFilter(field_name=filter_accessor, lookup_expr='gte', widget=forms.DateTimeInput(attrs={'class': 'form-control'}))
-        filter_to = filters.DateTimeFilter(field_name=filter_accessor, lookup_expr='lte', widget=forms.DateTimeInput(attrs={'class': 'form-control'}))
+        filter_from = filters.DateTimeFilter(field_name=filter_accessor, lookup_expr='gte', widget=forms.DateTimeInput(attrs={'class': 'form-control input-filter', 'placeholder': 'from'}))
+        filter_to = filters.DateTimeFilter(field_name=filter_accessor, lookup_expr='lte', widget=forms.DateTimeInput(attrs={'class': 'form-control input-filter', 'placeholder': 'to'}))
         column_filter = ColumnFilter({'date_from': filter_from, 'data_to': filter_to})
     elif isinstance(last_field, models.DecimalField):
-        filter_from = filters.NumberFilter(field_name=filter_accessor, lookup_expr='gte', widget=forms.NumberInput(attrs={'class': 'form-control'}))
-        filter_to = filters.NumberFilter(field_name=filter_accessor, lookup_expr='lte', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+        filter_from = filters.NumberFilter(field_name=filter_accessor, lookup_expr='gte', widget=forms.NumberInput(attrs={'class': 'form-control input-filter', 'placeholder': 'from'}))
+        filter_to = filters.NumberFilter(field_name=filter_accessor, lookup_expr='lte', widget=forms.NumberInput(attrs={'class': 'form-control input-filter', 'placeholder': 'to'}))
         column_filter = ColumnFilter({'number_from': filter_from, 'number_to': filter_to})
     elif isinstance(last_field, models.TextField):
-        text_filter = filters.CharFilter(field_name=filter_accessor, lookup_expr='icontains', widget=forms.Textarea(attrs={'class': 'form-control'}))
+        text_filter = filters.CharFilter(field_name=filter_accessor, lookup_expr='icontains', widget=forms.Textarea(attrs={'class': 'form-control input-filter'}))
         column_filter = ColumnFilter({'text_filter': text_filter})
     return column_filter
