3a2b05e9534f7b0659dfcf46a98c2f0e4ecb500d
Mohammad Khosravi <mammad.khosravi.78@gmail.com>
2022-11-07 11:16:55 +0330

    book example is updated to have custom column_filter

--- example/tables.py
+++ example/tables.py
@@ -1,5 +1,7 @@
 from django_table_filters.table_filters import Table, TableFilter, ColumnFilter, Column
 from example import models
+from django_filters import filters
+from django import forms
 
 
 class LibraryTable(Table):
@@ -23,6 +25,11 @@ class BookTable(Table):
 
 
 class BookTableFilter(TableFilter):
+    price = ColumnFilter({
+        'price_from': filters.NumberFilter(field_name='price', lookup_expr='gte', widget=forms.NumberInput(attrs={'class': 'form-control input-filter', 'placeholder': 'from'})),
+        'price_to': filters.NumberFilter(field_name='price', lookup_expr='lte', widget=forms.NumberInput(attrs={'class': 'form-control input-filter', 'placeholder': 'to'})),
+    })
+
     class Meta:
         table = BookTable
-        columns = ['name', 'code', 'Registration_Date', 'price', 'library', 'library__location', 'library__users',]
+        columns = ['name', 'code', 'Registration_Date', 'library', 'library__location', 'library__users', ]
